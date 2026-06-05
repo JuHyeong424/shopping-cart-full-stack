da480f32a42de0977e40afb3c66bb6f294f65808
JuHyeong424 <wkdwngud963@gmail.com>
2026-06-05 17:03:41 +0900

    feat: 카트 상품 api 관련 로직을 담은 커스텀훅

---
+++ client/src/pages/shoppingCart/hooks/useCartItems.ts
@@ -0,0 +1,148 @@
+import { useState, useEffect } from "react";
+import { BASE_URL } from "../constants/constant";
+
+interface ShoppingCartItem {
+  product: {
+    id: string;
+    image: string;
+    name: string;
+    price: number;
+  };
+  quantity: number;
+}
+
+export function useCartItems() {
+  const [shoppingCartItems, setShoppingCartItems] = useState<
+    ShoppingCartItem[]
+  >([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(`${BASE_URL}/carts`);
+        if (!response.ok) throw new Error("상품을 불러오지 못했습니다.");
+        const data: ShoppingCartItem[] = await response.json();
+        setShoppingCartItems(data);
+      } catch (error) {
+        console.error(error);
+        setError("상품 불러오기를 실패하였습니다. 다시 시도해주세요.");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleMinusQuantity = async (value: ShoppingCartItem) => {
+    const updatedQuantity = value.quantity - 1;
+
+    setShoppingCartItems((prev) => {
+      return prev.map((item) => {
+        return item.product.id === value.product.id
+          ? { ...item, quantity: updatedQuantity }
+          : item;
+      });
+    });
+
+    try {
+      const response = await fetch(`${BASE_URL}/carts/${value.product.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          quantity: value.quantity - 1,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("해당 상품의 수량을 변경하지 못했습니다.");
+      }
+    } catch (error) {
+      console.error("에러 발생", error);
+      setError("상품 수량은 1 이상 가능합니다. 다시 시도해주세요.");
+      setShoppingCartItems((prev) => {
+        return prev.map((item) => {
+          return item.product.id === value.product.id
+            ? { ...item, quantity: value.quantity }
+            : item;
+        });
+      });
+    }
+  };
+
+  const handlePlusQuantity = async (value: ShoppingCartItem) => {
+    const updatedQuantity = value.quantity + 1;
+    setShoppingCartItems((prev) => {
+      return prev.map((item) => {
+        return item.product.id === value.product.id
+          ? { ...item, quantity: updatedQuantity }
+          : item;
+      });
+    });
+
+    try {
+      const response = await fetch(`${BASE_URL}/carts/${value.product.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          quantity: value.quantity + 1,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("해당 상품의 수량을 변경하지 못했습니다.");
+      }
+    } catch (error) {
+      console.error("에러 발생", error);
+      setError("상품 수량은 99 이하 가능합니다. 다시 시도해주세요.");
+      setShoppingCartItems((prev) => {
+        return prev.map((item) => {
+          return item.product.id === value.product.id
+            ? { ...item, quantity: value.quantity }
+            : item;
+        });
+      });
+    }
+  };
+
+  const handleDeleteItem = async (value: ShoppingCartItem) => {
+    const prevItems = shoppingCartItems;
+
+    setShoppingCartItems((prev) => {
+      return prev.filter((item) => item.product.id !== value.product.id);
+    });
+
+    try {
+      const response = await fetch(`${BASE_URL}/carts/${value.product.id}`, {
+        method: "DELETE",
+      });
+
+      if (!response.ok)
+        throw new Error("장바구니 상품 삭제 중 오류가 발생했습니다.");
+
+      return true;
+    } catch (error) {
+      console.error("에러 발생", error);
+      setError("상품 삭제에 실패하였습니다. 다시 시도해주세요.");
+      setShoppingCartItems(prevItems);
+    }
+  };
+
+  return {
+    shoppingCartItems,
+    setShoppingCartItems,
+    isLoading,
+    setIsLoading,
+    error,
+    setError,
+    handleMinusQuantity,
+    handlePlusQuantity,
+    handleDeleteItem,
+  };
+}
