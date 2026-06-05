ac2f23c64a44be561253f40a9a069bfa34a6edb4
JuHyeong424 <wkdwngud963@gmail.com>
2026-06-05 16:15:55 +0900

    refactor: 상수 분리

---
+++ client/src/pages/shoppingCart/constants/constant.ts
@@ -0,0 +1,11 @@
+export const UN_CHECKED =
+  "data:image/svg+xml,%3csvg%20width='24'%20height='24'%20viewBox='0%200%2024%2024'%20fill='none'%20xmlns='http://www.w3.org/2000/svg'%3e%3crect%20width='24'%20height='24'%20rx='8'%20fill='white'/%3e%3crect%20x='0.5'%20y='0.5'%20width='23'%20height='23'%20rx='7.5'%20stroke='black'%20stroke-opacity='0.1'/%3e%3cg%20clip-path='url(%23clip0_13996_1608)'%3e%3cpath%20d='M9%2016.17L4.83%2012L3.41%2013.41L9%2019L21%207L19.59%205.59L9%2016.17Z'%20fill='black'%20fill-opacity='0.1'/%3e%3c/g%3e%3cdefs%3e%3cclipPath%20id='clip0_13996_1608'%3e%3crect%20width='24'%20height='24'%20fill='white'/%3e%3c/clipPath%3e%3c/defs%3e%3c/svg%3e";
+
+export const CHECKED =
+  "data:image/svg+xml,%3csvg%20width='24'%20height='24'%20viewBox='0%200%2024%2024'%20fill='none'%20xmlns='http://www.w3.org/2000/svg'%3e%3crect%20x='0.5'%20y='0.5'%20width='23'%20height='23'%20rx='7.5'%20fill='black'%20stroke='black'/%3e%3cg%20clip-path='url(%23clip0_15620_131)'%3e%3cpath%20d='M9%2016.17L4.83%2012L3.41%2013.41L9%2019L21%207L19.59%205.59L9%2016.17Z'%20fill='white'/%3e%3c/g%3e%3cdefs%3e%3cclipPath%20id='clip0_15620_131'%3e%3crect%20width='24'%20height='24'%20fill='white'/%3e%3c/clipPath%3e%3c/defs%3e%3c/svg%3e";
+
+export const BASE_URL = import.meta.env.VITE_API_URL;
+
+export const STORAGE_KEY = "cart-checked-ids";
+
+export const DELIVERY_FEE = 3000;
