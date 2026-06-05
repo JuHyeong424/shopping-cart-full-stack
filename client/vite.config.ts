85040eb307f6dcacab5ba3c75a5a0722079315cc
JuHyeong424 <wkdwngud963@gmail.com>
2026-06-05 17:39:40 +0900

    chore: 깃허브 배포 설정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/shopping-cart-full-stack/",
   plugins: [react()],
   test: {
     globals: true,
