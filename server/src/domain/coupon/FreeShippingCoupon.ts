20443ad02bcf2750880e6c5a8c1a5a9dd7438cbb
JuHyeong424 <wkdwngud963@gmail.com>
2026-06-18 14:09:03 +0900

    feat: 무료 배송 쿠폰 도메인 클래스 생성

---
+++ server/src/domain/coupon/FreeShippingCoupon.ts
@@ -0,0 +1,10 @@
+import Coupon, { type CouponContext, type DiscountCategory } from "./Coupon.ts";
+
+/** 배송비를 전액(도서산간 추가 배송비 포함) 무료로 만드는 쿠폰. */
+export default class FreeShippingCoupon extends Coupon {
+  readonly category: DiscountCategory = "SHIPPING";
+
+  discount(context: CouponContext): number {
+    return context.shippingFee;
+  }
+}
